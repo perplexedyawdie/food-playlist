9e72c26b754c903a6d8379d7030f480881547e60
perplexedyawdie <sunkwenga@gmail.com>
2022-06-04 22:57:55 -0500

    minor fixes

--- components/CreatePlaylist.tsx
+++ components/CreatePlaylist.tsx
@@ -8,7 +8,7 @@ const CreatePlaylist = () => {
                 <label htmlFor='' className="modal-box relative border-black bg-[#dbec79] rounded-none border-4 drop-shadow-[4px_4px_rgba(0,0,0,1)] text-black">
                 <label htmlFor="create-playlist-modal" className="btn btn-sm btn-square bg-[#ff7d85] border-black rounded-none border-4 drop-shadow-[2px_2px_rgba(0,0,0,1)] absolute right-2 top-2 text-black hover:text-[#ff7d85]">✕</label>
                     <h3 className="font-bold text-lg mt-8">Congratulations random Interner user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <p className="py-4">You&apos;ve been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                     <div className="modal-action">
                         <label htmlFor="my-modal" className="btn">Yay!</label>
                     </div>
